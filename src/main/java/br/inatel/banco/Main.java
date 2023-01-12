f626708608e774ea9cec0d9d44614bd5a6a6b7e2
“Dimitri” <dimitri.s.amado@gmail.com>
2023-01-11 21:18:23 -0300

    Building usuary menu and integration tests

--- src/main/java/br/inatel/banco/Main.java
+++ src/main/java/br/inatel/banco/Main.java
@@ -1,5 +1,6 @@
 package br.inatel.banco;
 
+import br.inatel.banco.controller.ManipulaConta;
 import br.inatel.banco.services.ContaBaseServices;
 import br.inatel.banco.services.ContaCorrenteService;
 import br.inatel.banco.services.ContaPoupancaService;
@@ -8,6 +9,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("----------Inatel Banking----------");
 
+        ManipulaConta Conta = new ManipulaConta();
+
+        Conta.menuInicial();
+        /*
         ContaCorrenteService ContaCorrente = new ContaCorrenteService("1111", "1234567-8");
 
         ContaPoupancaService ContaPoupanca = new ContaPoupancaService("2222", "9876543-1");
@@ -29,6 +34,6 @@ public class Main {
         ContaPoupanca.pagarConta(99.90, "Internet");
         ContaPoupanca.adicionaRendimento();
         System.out.println("Rendimento: " + ContaPoupanca.consultaRendimento());
-        ContaPoupanca.consultaExtrato();
+        ContaPoupanca.consultaExtrato(); */
     }
 }
